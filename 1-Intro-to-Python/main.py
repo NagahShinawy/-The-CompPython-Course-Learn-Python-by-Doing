7decd96dbb7ada2a356dfaf268a40d3eaa9f154c
Nagah <E-n.Shinawy@lean.sa>
2021-09-13 10:19:37 +0300

    numbers

--- 1-Intro-to-Python/main.py
+++ 1-Intro-to-Python/main.py
@@ -1,7 +1,6 @@
 # ############ ############  VARIABLES ############ ############ ###########
 print("# ############ ############  VARIABLES ############ ############ ###########")
 
-
 age = 21
 
 print(f"Age: {age}")
@@ -23,3 +22,38 @@ PI = 3.14
 REJECTED = "Rejected"
 PASSED = "Passed"
 PENDING = "Pending"
+
+#  ############ ############  NUMBERS ############ ############ ###########
+
+print("# ############ ############  numbers ############ ############ ###########")
+purchases_items = 4
+
+price = 25
+
+total = purchases_items * price
+
+print(f"Total: {total}")
+
+TAX = 0.15
+
+tax = total * TAX
+
+print(f"Tax: {tax}")
+
+total += tax
+
+print(f"Price After Taxs is {total}")
+
+print(10 // 3)  # Floor division
+
+print(10 % 3)  # reminder
+
+
+result = 10 / 3
+to_int = 10 // 3
+
+decimal_place = result - to_int
+
+print(f"Result: {result}")
+print(f"To int: {to_int}")
+print(f"Decimal Place: {decimal_place}")
