ea09b81efb6fc05812f4b542e74e1adcf23bca4b
Nagah <E-n.Shinawy@lean.sa>
2021-09-15 12:26:10 +0300

    list methods

--- 1-Intro-to-Python/3-lists.py
+++ 1-Intro-to-Python/3-lists.py
@@ -37,5 +37,54 @@ if loen in students:
     print(f"Removing {loen}")
     students.remove(loen)
 
-
 print(students)
+
+# insert item at index
+
+
+emails = [
+    "test@outlook.com",  # 0
+    "james@outlook.com",  # 1
+    "sara@outlook.com",  # 2
+    "john@outlook.com",  # 3
+]
+
+print(emails)
+
+emails.insert(2, "adam@outlook.com")
+
+print(emails)
+
+# pop: remove item by index and return removed item
+
+sara = emails.pop(2)
+
+print(sara)
+
+print(emails)
+
+item = emails.pop()  # default index is -1, so "john@outlook.com" was deleted
+
+print(item)
+
+print(emails)
+
+# print(emails.pop(12))  # IndexError: pop index out of range
+# print(emails.pop(-12))  # IndexError: pop index out of range
+
+
+# clear
+
+evens = [4, 10, 2, 22, 24, 26]
+
+print(evens)
+
+evens.clear()
+
+print(evens)
+
+evens = [4, 10, 2, 22, 24, 26]
+print(evens)
+evens = []
+
+print(evens)
