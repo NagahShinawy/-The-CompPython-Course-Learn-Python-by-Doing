33dd3dac507dc5892b25ababb684867635a059ad
Nagah <E-n.Shinawy@lean.sa>
2021-09-15 13:58:09 +0300

    set operations

--- 1-Intro-to-Python/5-sets.py
+++ 1-Intro-to-Python/5-sets.py
@@ -1,3 +1,5 @@
+# https://www.teclado.com/30-days-of-python/python-30-day-11-sets
+
 grades = [45, 23, 45, 90, 100, 50, 40, 90]
 
 order = ["checking", "meal", "meal", "checking", "burger"]
@@ -8,7 +10,7 @@ unique = set(order)
 
 print(unique)
 
-salaries = [("hr", 4000), ("it", 2000), ("accounting", 2000), ("finance", 2000)]
+salaries = [("hr", 17000), ("it", 2000), ("accounting", 2000), ("finance", 2000)]
 
 salaries = [salary[-1] for salary in salaries]
 
@@ -24,16 +26,32 @@ print(unique_salaries)
 
 unique_salaries.add(3000)
 unique_salaries.add(3000)
-unique_salaries.add(3000)
+unique_salaries.add("3000")
+unique_salaries.add(3000.01)
+
+# items in sets must be immutables like string, int, float
+# unique_salaries.add([])  # TypeError: unhashable type: 'list'
 
 
+# remove
 unique_salaries.remove(3000)
 # unique_salaries.remove(3001)  # KeyError: 3001: must be member
 
 print(unique_salaries)
 
 
+print(unique_salaries)
 
+
+# update
+unique_salaries.update([3000, 5000, 5000])
 
 print(unique_salaries)
 
+# print(unique_salaries[0])  # TypeError: 'set' object is not subscriptable
+x = 17000
+random_salary = unique_salaries.pop()
+for _ in range(1000000):
+    pass
+    # assert x == random_salary, f"{random_salary}"
+print(random_salary)
