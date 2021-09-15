a833be848fd03ca25d02eaf6211e87d285b2db69
Nagah <E-n.Shinawy@lean.sa>
2021-09-15 11:52:23 +0300

    bool tips and tricks

--- 1-Intro-to-Python/bool.py
+++ 1-Intro-to-Python/bool.py
@@ -1,3 +1,5 @@
+# https://blog.teclado.com/logical-comparisons-in-python-and-or/
+
 
 print(bool(35))   # True
 print(bool(""))   # False
@@ -5,7 +7,7 @@ print(bool(""))   # False
 print(bool([]))  # False
 print(bool([""]))   # True
 
-print(35 and 1)  # 1 means True
+print(35 and 10)  # 10 returns second value
 print(35 and 0)   # 0 means False
 
 
@@ -16,4 +18,29 @@ surname = "James"
 welcome = name or f"{surname}"
 
 print(welcome)
+
+
+created = True
+
+updated = not created
+
+
+print(created)
+print(updated)
+
+
+print(None and 35)  # None
+print(None or 35)  # 35
+
+print(10 and 0)  # 0 ==> second value.
+
+
+print(20 and [])  # [] second value because we are using and
+print(20 or [])  # 20 first value because we are using or
+
+
+
+
+
+
 
