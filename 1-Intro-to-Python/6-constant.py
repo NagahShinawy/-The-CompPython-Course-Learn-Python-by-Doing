720a55889b69bc2d18bc5fd661f53cb033db4937
Nagah <E-n.Shinawy@lean.sa>
2021-09-16 16:15:53 +0300

    constants

---
+++ 1-Intro-to-Python/6-constant.py
@@ -0,0 +1,19 @@
+UNDERAGE = 18
+
+RETIREMENT_AGE = 65
+
+BLOOD_TYPES = ("A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "0-")
+
+LEVELS = ("Junior", "Senior", "Team Lead")
+
+
+class Status:
+
+    NEW = "New"
+    APPROVED = "Approved"
+    CANCELED = "Canceled"
+    REJECTED = "Rejected"
+
+
+print(Status.NEW)
+print(Status.REJECTED)
