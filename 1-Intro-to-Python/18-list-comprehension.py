81b6a0ad9cfa281778cd321dc8a557cd2b3772c8
Nagah <E-n.Shinawy@lean.sa>
2021-09-17 21:37:13 +0300

    comprehensions list, set, dict

--- 1-Intro-to-Python/18-list-comprehension.py
+++ 1-Intro-to-Python/18-list-comprehension.py
@@ -1,5 +1,9 @@
 # https://blog.teclado.com/python-list-comprehensions/
 
+
+# https://www.teclado.com/30-days-of-python/python-30-day-15-comprehensions
+
+
 numbers = [6, 7, 3, 10, 20]
 
 doubles = [number * 2 for number in numbers]
@@ -14,5 +18,38 @@ def get_name(email: str):
 emails = ["john@test.com", "james@test.com", "loen@test.com", "adam@test.com"]
 
 names = [get_name(email).title() for email in emails]
+
+print(names)
+
+
+names = ("mary", "Richard", "Noah", "KATE")
+ages = (36, 21, 40, 28)
+
+people = [(name.title(), age) for name, age in zip(names, ages)]
+
+print(people)
+
+# Set comprehensions
+
+names = [
+    "john",
+    "loen",
+    "smith",
+    "bob",
+    "anna",
+    "james",
+    "adam",
+    "james",
+    "john",
+    "sara",
+    "adam",
+]
+
+unique_names = {name.title() for name in names}
+
+print(len(names))  # 11
+print(len(unique_names))  # 8
+
+names = {name: len(name) for name in names}
 
 print(names)
