bd581bd5e0a47fe89da95430862588e427c1a96d
Nagah <E-n.Shinawy@lean.sa>
2021-09-17 17:51:35 +0300

    if

---
+++ 1-Intro-to-Python/10-if.py
@@ -0,0 +1,45 @@
+username = input("Enter username: ")
+
+ADMIN = "Admin"
+
+if username == ADMIN:
+    print("You Have All Permissions!")
+
+
+if username != ADMIN:
+    print("You are not superuser")
+
+
+class SpeedStatus:
+    UNKNOWN = "Unknown"
+    SLOW = "Slow"
+    NORMAL = "Normal"
+    WARNING = "Warning, Please Slow down"
+    DANGER = "Danger speed, Stopping the car"
+
+
+SLOW_SPEED = range(1, 41)
+NORMAL_SPEED = range(40, 101)
+WARNING_SPEED = range(100, 161)
+DANGER_SPEED = range(161, 201)
+
+speed = 130
+
+if speed in SLOW_SPEED:
+    print(SpeedStatus.SLOW)
+
+elif speed in NORMAL_SPEED:
+    print(SpeedStatus.NORMAL)
+
+elif speed in WARNING_SPEED:
+    print(SpeedStatus.WARNING)
+
+elif speed in DANGER_SPEED:
+    print(SpeedStatus.DANGER)
+
+else:
+    print(SpeedStatus.UNKNOWN)
+
+
+
+
