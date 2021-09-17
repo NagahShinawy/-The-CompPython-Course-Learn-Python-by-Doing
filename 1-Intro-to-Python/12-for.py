8b252b4c58ef20e93473af17df3b664085894600
Nagah <E-n.Shinawy@lean.sa>
2021-09-17 18:28:19 +0300

    for

---
+++ 1-Intro-to-Python/12-for.py
@@ -0,0 +1,22 @@
+themes = ["Black", "White", "Green", "Gray"]
+
+for theme in themes:
+    print(theme)
+
+
+for even in range(0, 11, 2):
+    print(even, end=" ")
+
+print()
+print("#" * 100)
+
+
+booking_history = [
+    {"from": "Cairo", "to": "Alex", "date": "2021-01-01", "price": "10$"},
+    {"from": "Cairo", "to": "Gadda", "date": "2021-01-02", "price": "500$"},
+    {"from": "Cairo", "to": "Dubai", "date": "2021-01-03", "price": "600$"},
+]
+
+print("From\tTo")
+for booking in booking_history:
+    print(f"{booking['from']}\t{booking['to']}")
