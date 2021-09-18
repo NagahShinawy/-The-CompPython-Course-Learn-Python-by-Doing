ee70e790a7164852f44bb9e67c51f4303fb00b78
Nagah <E-n.Shinawy@lean.sa>
2021-09-18 17:05:52 +0300

    local() vs global

--- 1-Intro-to-Python/24-functions.py
+++ 1-Intro-to-Python/24-functions.py
@@ -1,3 +1,6 @@
+# https://www.teclado.com/30-days-of-python/python-30-day-13-return-statements
+
+
 cars = [
     {"make": "Ford", "model": "Fiesta", "mileage": 23000, "fuel_consumed": 460},
     {"make": "Ford", "model": "Focus", "mileage": 17000, "fuel_consumed": 350},
@@ -7,6 +10,7 @@ cars = [
 
 
 def welcome(name: str) -> str:
+    print(locals())  # {'name': 'John'}
     return f"Welcome, {name}"
 
 
@@ -32,6 +36,10 @@ def main():
         mpg = calc_mpg(car)
         print(f"{mpg:,.3f}")
 
+    print(globals())
+    print(locals())
+
 
 if __name__ == "__main__":
     main()
+    # print(locals())
