900de42d4f75612a0c80fb4787b5b00925ddea9d
Nagah <E-n.Shinawy@lean.sa>
2021-09-21 14:14:47 +0300

    calc weekly working hours for working student

--- 3-oop/5-inheritance.py
+++ 3-oop/5-inheritance.py
@@ -1,5 +1,4 @@
 class Student:
-
     def __init__(self, name, school):
         self.name = name
         self.school = school
@@ -17,18 +16,25 @@ class Student:
 
 class WorkingStudent(Student):
 
+    WEEKLY_WORKING_HOURS = 37.5
+    DOLLAR = "$"
+
     def __init__(self, name, school, salary):
         super().__init__(name, school)
         self.salary = salary
 
+    @property
+    def weekly_salary(self):
+        return f"{self.salary * self.WEEKLY_WORKING_HOURS}{self.DOLLAR}"
+
 
 def main():
     james = Student("James", "MIT")
-    adam = WorkingStudent("Adam", "MIT", 1000)
+    adam = WorkingStudent("Adam", "Oxford", 50)
     print(james)
     print(adam)
+    print(adam.weekly_salary)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
