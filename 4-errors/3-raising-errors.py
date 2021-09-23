2ced57715ca322d508fb5b4c395b62f973855b79
Nagah <E-n.Shinawy@lean.sa>
2021-09-23 18:36:53 +0300

    __repre__ car

--- 4-errors/3-raising-errors.py
+++ 4-errors/3-raising-errors.py
@@ -9,7 +9,7 @@ class BaseError(Exception):
 
 
 class CarAlreadyExist(BaseError):
-    text = "Car <{} {}>  already exist"
+    text = "Car <{}>  already exist"
     description = None
     code = None
 
@@ -18,6 +18,9 @@ class Car:
     def __init__(self, model, price):
         self.model = model
         self.price = price
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.model}, {self.price})"
 
 
 class Garage:
@@ -30,12 +33,14 @@ class Garage:
     def add_car(self, car):
         if car in self.cars:
             error = CarAlreadyExist()
-            error.raise_error(car.model, car.price)
+            error.raise_error(car)
         self.cars.append(car)
 
 
 if __name__ == "__main__":
     garage = Garage()
     bwm = Car("BMW", "120K")
+    nissan = Car("Nissan", "100K")
     garage.add_car(bwm)
-    garage.add_car(bwm)
+    garage.add_car(nissan)
+    # garage.add_car(nissan)
