841d91d3d3a89164032f8d200b9cf3c52d1b3059
Nagah <E-n.Shinawy@lean.sa>
2021-09-23 18:18:36 +0300

    most common errors-2

--- 4-errors/2-builtin-errors.py
+++ 4-errors/2-builtin-errors.py
@@ -55,6 +55,7 @@ john = Profile("johnsmith")
 # todo#6 ValueError
 
 # print(int("test"))  # ValueError: invalid literal for int() with base 10: 'test'
+# print(int("20.5"))  # ValueError: invalid literal for int() with base 10: 'test'
 
 
 # ######## ######## ######## ######## ######## ######## ######## ######## #######
@@ -62,3 +63,73 @@ john = Profile("johnsmith")
 # todo#7 TypeError
 
 # print("Test" + 12)  # TypeError: can only concatenate str (not "int") to str
+
+
+# ######## ######## ######## ######## ######## ######## ######## ######## #######
+
+# todo#8 ImportError
+
+# from . import *  # ImportError: attempted relative import with no known parent package
+
+# ######## ######## ######## ######## ######## ######## ######## ######## #######
+
+# todo#9 NotimplementedError
+
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return self.username.title()
+
+    def login(self):
+        raise NotImplementedError("Login feature is not implemented yet")
+
+
+james = User("james", "james@123")
+
+print(james)
+
+# james.login() # NotImplementedError: Login feature is not implemented yet
+
+
+# ######## ######## ######## ######## ######## ######## ######## ######## #######
+
+# todo# SyntaxError
+
+
+def add(x, y):
+    pass
+
+
+# return x + y  # SyntaxError: 'return' outside function
+
+
+# ######## ######## ######## ######## ######## ######## ######## ######## #######
+
+# todo#10 TabError
+
+
+# ######## ######## ######## ######## ######## ######## ######## ######## #######
+
+
+def power(number: int, exp: int):
+    return number ** exp  # 2 ^ 4
+
+
+print(power(2, 4))  # 2 ^ 4 = 16
+
+
+# ######## ######## ######## ######## ######## ######## ######## ######## #######
+
+# todo#11 ModuleNotFoundError
+
+# import testing  # ModuleNotFoundError: No module named 'testing'
+
+
+# ######## ######## ######## ######## ######## ######## ######## ######## #######
+
+# todo#12 DeprecationWarning
+# means : it is no longer the best way (the accepted way) to doing something
