0e19ef61133af74941e4278a4ad05211eeaef4e9
Nagah <E-n.Shinawy@lean.sa>
2021-09-23 18:46:32 +0300

    handling adding not Car obj to Garage

--- 4-errors/3-raising-errors.py
+++ 4-errors/3-raising-errors.py
@@ -14,6 +14,12 @@ class CarAlreadyExist(BaseError):
     code = None
 
 
+class NotCarObj(BaseError):
+    text = "Value of <{}> is not <{}> obj. it is <{}>."
+    description = None
+    code = None
+
+
 class Car:
     def __init__(self, model, price):
         self.model = model
@@ -34,7 +40,13 @@ class Garage:
         if car in self.cars:
             error = CarAlreadyExist()
             error.raise_error(car)
+        if not isinstance(car, Car):
+            error = NotCarObj()
+            error.raise_error(car, Car.__name__, car.__class__.__name__)
         self.cars.append(car)
+
+    def remove_car(self, car):
+        raise NotImplementedError("Can not remove cars!. Not implemented Yet")
 
 
 if __name__ == "__main__":
@@ -43,4 +55,6 @@ if __name__ == "__main__":
     nissan = Car("Nissan", "100K")
     garage.add_car(bwm)
     garage.add_car(nissan)
-    # garage.add_car(nissan)
+    # garage.add_car(nissan)  # __main__.CarAlreadyExist: Car <Car(Nissan, 100K)>  already exist
+    # garage.remove_car(nissan)  # NotImplementedError: Can not remove cars!. Not implemented Yet
+    # garage.add_car("kia")  # __main__.NotCarObj: Value of <kia> is not <Car> obj. it is <str>
