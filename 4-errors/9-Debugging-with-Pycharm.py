0afd4c0574764d09fff8a0c09234a5fa24baa7a6
Nagah <E-n.Shinawy@lean.sa>
2021-09-23 21:26:37 +0300

    debug

---
+++ 4-errors/9-Debugging-with-Pycharm.py
@@ -0,0 +1,7 @@
+# step over: skipp , go with no details
+
+# step into : go in details
+# step into my code : go in details with my code
+# step out: exist / break.
+
+# resume: continue running and show final output.
